063769cebab72a3f2e03c7257e2f13616d21541e
Michael Spohn <mikeLspohn@gmail.com>
2015-09-13 11:57:23 -0500

    Add navigator geolocation marker after current location is found

--- client/busICT.js
+++ client/busICT.js
@@ -6,7 +6,13 @@ var routeLayers = {};
 
 Template.body.helpers({
   routes: routes,
-  stops: stops
+  stops: stops,
+  lat: function() {
+    return Session.get('lat');
+  },
+  lon: function() {
+    return Session.get('lon');
+  }
 })
 
 Template.body.events({
@@ -26,11 +32,29 @@ Template.body.events({
 
 // When Template.map is rendered run this
 Template.map.rendered = function() {
+  // var lat = Session.get('lat');
+  // var lon = Session.get('lon');
+  //
+  // var currentLocation = [lat, lon];
+
   L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
 
   map = L.map('map', {
     doubleClickZoom: false
-  }).setView(startingLocation, 11);
+  }).setView(startingLocation, 15);
+
+  // Session.set('map', map)
+  // goToCurrentLocation();
+
+  // L.marker(currentLocation).addTo(map);
+  
+  //
+  // L.circle(currentLocation, 500, {
+  //   color: 'red',
+  //   fillColor: '#f03',
+  //   fillOpacity: 0.5
+  // }).addTo(map);
+
 
   // Set the "Theme" for the map. Other nice options are:
   // Thunderforest.Transport
@@ -55,6 +79,24 @@ Template.map.rendered = function() {
     L.marker([lat, lon], { title: title_text  }).addTo(map);
   })
 
+  //pan to current location after it's found
+  navigator.geolocation.watchPosition(function(position) {
+
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+
+    map.panTo([lat, lon]);
+
+    var marker = L.marker([lat, lon]).addTo(map);
+    // var circle = L.circle([lat, lon], 100, {
+    //   color: 'red',
+    //   fillColor: '#f03',
+    //   fillOpacity: 0.5
+    // }).addTo(map);
+
+    marker.bindPopup('<b>This is you!</b><br>All stops are marked on your map').openPopup();
+  })
+
   // Set a window resize listener to set the map to the height of the
   // viewable area then force a resize for the initial load
   $(function() {
@@ -65,3 +107,4 @@ Template.map.rendered = function() {
     $(window).resize(); // trigger resize event
   })
 };
+
